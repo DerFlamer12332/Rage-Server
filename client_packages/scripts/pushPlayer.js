7cbc5ae31bd88e5db9c63b05686a39f1594cea29
DerFlamer12332 <46220338+DerFlamer12332@users.noreply.github.com>
2020-01-13 06:05:55 +0100

    Update pushPlayer.js

--- client_packages/scripts/pushPlayer.js
+++ client_packages/scripts/pushPlayer.js
@@ -2,12 +2,14 @@
 
 						// Komma - Verbandskasten ziehen 
 	mp.keys.bind(0xBC, true, () => {
-
+	mp.game.streaming.requestAnimDict("random@shop_robbery");
+	mp.players.local.taskPlayAnim("random@shop_robbery", "robbery_action_f", 8.0, 1.0, 5000, 1, 1.0, false, false, false);
 	setTimeout(function () {
-           mp.events.callRemote("Verband");
-        }, 5000);
+        mp.events.callRemote("Verband");
+	
+       }, 5000);
 
-    	
+   	
 });
 
 
@@ -15,11 +17,14 @@
 
 						// Punkt - Weste ziehen 
 
+
 	mp.keys.bind(0xBE, true, () => {
-
+	mp.game.streaming.requestAnimDict("random@shop_robbery");
+	mp.players.local.taskPlayAnim("random@shop_robbery", "robbery_action_f", 8.0, 1.0, 5000, 1, 1.0, false, false, false);
 	setTimeout(function () {
-           mp.events.callRemote("Weste");
-        }, 5000);
+        mp.events.callRemote("Weste");
+	
+       }, 5000);
 
-    	
+   	
 });
